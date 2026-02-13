a4561558a8f24f27b97b53fe1b5b625bcc3f386c
sudhanshu24122002-lang <sudhanshu24122002@gmail.com>
2026-02-13 10:38:48 +0530

    Refactor animation logic for skill bars

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
           {
             width:
               $(this)
-                .parents()
+                .parent()
                 .attr("data-percent") + "%"
           },
           1000,
@@ -22,5 +22,49 @@
         );
     });
 
+  $(".skills-soft li")
+    .find("svg")
+    .each(function(i) {
+      var c, cbar, circle, percent, r;
+      circle = $(this).children(".cbar");
+      r = circle.attr("r");
+      c = Math.PI * (r * 2);
+      percent = $(this)
+        .parent()
+        .data("percent");
+      cbar = (100 - percent) / 100 * c;
+      circle.css({
+        "stroke-dashoffset": c,
+        "stroke-dasharray": c
+      });
+      circle.delay(i * 150).animate(
+        {
+          strokeDashoffset: cbar
+        },
+        1000,
+        "linear",
+        function() {
+          return circle.css({
+            "transition-duration": ".3s"
+          });
+        }
+      );
+      $(this)
+        .siblings("small")
+        .prop("Counter", 0)
+        .delay(i * 150)
+        .animate(
+          {
+            Counter: percent
+          },
+          {
+            duration: 1000,
+            step: function(now) {
+              return $(this).text(Math.ceil(now) + "%");
+            }
+          }
+        );
+    });
+}.call(this));
 
 
